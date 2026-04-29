c5c2931cad5ea60ebdd0bfe01e4c7f8a204292ab
Juan Pablo C <jpcruzvargas@estudiantec.cr>
2026-04-28 19:43:39 -0600

    Adición de 5 pruebas distintas con todos los plusargs

--- tb/comando.sh
+++ tb/comando.sh
@@ -5,11 +5,12 @@ vcs -Mupdate tb_top.sv  -o salida  -full64 -sverilog  -kdb -lca -debug_acc+all -
 
 #alias home_dir='/mnt/vol_NFS_rh003/Est_Veri_I_2026/CRUZ_JUAN'
 
-./salida 
-#./salida +MODO=1 #OVERFLOW
-#./salida +MODO=2 #UNDERFLOW
-#./salida +MODO=3 #RESET
-#./salida +MODO=1 #SIMULTANEO write/read
+#./salida 
+#./salida +MODO=OVERFLOW +NUM_TRANS=1000 +MAX_RETARDO=1 +MIN_RETARDO=0 #OVERFLOW
+./salida +MODO=SIM_BAJO +NUM_TRANS=80 +MAX_RETARDO=15 +MIN_RETARDO=5 #Baja latencia
+#./salida +MODO=UNDERFLOW +NUM_TRANS=100 +SEED=$RANDOM #UNDERFLOW
+#./salida +MODO=RESET_LLENA +NUM_TRANS=50 +SEED=12345 #RESET llena con seed especifica
+#./salida +MODO=SIM_ALTO +NUM_TRANS=20 +MIN_RETARDO=0 +MAX_RETARDO=0 +SEED=777 #SIMULTANEO alta latencia
 
 
 
